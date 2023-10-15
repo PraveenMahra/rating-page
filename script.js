ea2678d016fcb8335841401030491bb86d3a24fc
Praveen singh mahra <Praveen.mehra65@gmail.com>
2023-10-15 17:45:02 +0530

    js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const ratingLabels = document.querySelectorAll(".rating-count");
-  const thankYouContainer = document.querySelector(".thanku-container");
-  const givenRatingText = document.querySelector(".given-rating");
-  const form = document.querySelector("form");
-  const container = document.querySelector(".container");
+const ratingLabels = document.querySelectorAll(".rating-count");
+const thankYouContainer = document.querySelector(".thanku-container");
+const givenRatingText = document.querySelector(".given-rating");
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
 
-  let selectedRating = 0;
+let selectedRating = 0;
 
-  // Add event listeners to each rating label
-  ratingLabels.forEach((label, index) => {
-    label.addEventListener("click", () => {
-      selectedRating = index + 1;
-      label.style.backgroundColor = "var(--primary-orange)";
-      label.style.color = "var(--white)";
-      updateSelectedRatingText();
-    });
+// Add event listeners to each rating label
+ratingLabels.forEach((label, index) => {
+  label.addEventListener("click", () => {
+    selectedRating = index + 1;
+    label.style.backgroundColor = "var(--primary-orange)";
+    label.style.color = "var(--white)";
+    updateSelectedRatingText();
   });
+});
 
-  // Function to update the selected rating text
-  function updateSelectedRatingText() {
-    givenRatingText.textContent = `You selected ${selectedRating} out of 5`;
-  }
+// Function to update the selected rating text
+function updateSelectedRatingText() {
+  givenRatingText.textContent = `You selected ${selectedRating} out of 5`;
+}
 
-  // Add an event listener for the form submission
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
+// Add an event listener for the form submission
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    thankYouContainer.classList.remove("hidden");
+  thankYouContainer.classList.remove("hidden");
 
-    container.classList.add("hidden");
-  });
+  container.classList.add("hidden");
 });
